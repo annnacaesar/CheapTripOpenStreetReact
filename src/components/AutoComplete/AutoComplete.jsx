d97974ed07a0b09462e2a0b5d627b4b88ed86adc
annnacaesar <Anechka22334@gmail.com>
2022-12-17 16:36:37 +0200

    task

--- src/components/AutoComplete/AutoComplete.jsx
+++ src/components/AutoComplete/AutoComplete.jsx
@@ -1,34 +1,42 @@
-import "./AutoComplete.css"
-import {useState} from "react"
+import './AutoComplete.css';
+import { useState } from 'react';
+import AutoCompleteItem from './AutoCompleteItem';
 export default function AutoComplete({
-  value,
-  setValue,
-  placeholder,
-  onChange,
-  options,
-  setOptions
+	value,
+	setValue,
+	placeholder,
+	onChange,
+	options,
+	setOptions,
+	resultClick,
 }) {
-  const [optionsActive,setOptionsActive] = useState(false);
-  return (
-    <div className="autoComplete">
-      <input
-        className="input"
-        placeholder={placeholder}
-        value={value}
-        onChange={onChange}
-        onClick={!optionsActive && setOptionsActive(true)}
-        onBlur={()=>{
-          setTimeout(()=>{
-            setOptions([])
-          },200);
-        }}
-      />
-      <ul>
-        {options && options.map(option =><li onClick={()=>{
-          setValue(option);
-          setOptions([]);
-        }}>{option}</li>)}
-      </ul>
-    </div>
-  );
+	const [optionsActive, setOptionsActive] = useState(false);
+	return (
+		<div className="autoComplete">
+			<input
+				className="input"
+				placeholder={placeholder}
+				value={value}
+				onChange={onChange}
+				onClick={!optionsActive ? setOptionsActive(true) : undefined}
+				onBlur={() => {
+					setTimeout(() => {
+						// setOptions([])
+					}, 200);
+				}}
+			/>
+			<ul>
+				{options &&
+					options.map(option => (
+						<AutoCompleteItem
+							key={option.id}
+							option={option}
+							setValue={setValue}
+							setOptions={setOptions}
+							resultClick={resultClick}
+						/>
+					))}
+			</ul>
+		</div>
+	);
 }
---
+++ src/components/AutoComplete/AutoCompleteItem.jsx
@@ -0,0 +1,24 @@
+export default function AutoCompleteItem({
+	option,
+	setValue,
+	setOptions,
+	resultClick,
+}) {
+	return (
+		<li
+			className="item"
+			// key={option.id}
+			onClick={() => {
+				setValue(option.city);
+				setOptions([]);
+				resultClick({
+					geometry: option.geometry,
+					display_name: option.city,
+				});
+			}}
+		>
+			<p className="itemCity">{option.city}</p>
+			<p className="itemCountry">{option.country}</p>
+		</li>
+	);
+}
